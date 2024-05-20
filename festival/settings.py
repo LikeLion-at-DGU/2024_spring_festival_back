6c617da98ccb97451c713bb3c93facabdae2c2d8
JongbeomLee623 <jambon23@dgu.ac.kr>
2024-05-20 20:58:34 +0900

    ✨ Refactor: BaseImage 저장 경로 수정 및 미디어 설정
    경로를 모델 명에 따라가게 저장하도록 했습니다. 그와 더불어 media 설정하여 올립니다.

--- festival/settings.py
+++ festival/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,7 +49,7 @@ INSTALLED_APPS = [
     'notice',
     'promo',
     'timetable',
-    
+
 
 ]
 
@@ -149,3 +150,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
