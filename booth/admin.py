10143e7419491443fd6eba1040ba172eb4d8655f
JongbeomLee623 <jambon23@dgu.ac.kr>
2024-05-26 17:15:19 +0900

    🚑️ Fix: booth admin 페이지 conflict 수정
    merge가 덜 됐어서 은호 변경사항이 적용되지 않은채로 올려서 오류가 났던것 같습니다. 수정해서 다시 올립니다.

--- booth/admin.py
+++ booth/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Booth, BoothOperationTime, BoothLike, BoothImage, Comment
+from .models import Booth, BoothLocationOperationTime, BoothLike, BoothImage, Comment
 
-class BoothOperationTimeInline(admin.TabularInline):
-    model = BoothOperationTime
+class BoothLocationOperationTimeInline(admin.TabularInline):
+    model = BoothLocationOperationTime
     extra = 1
 
 class BoothImageInline(admin.TabularInline):
@@ -10,13 +10,13 @@ class BoothImageInline(admin.TabularInline):
     extra = 1
 
 class BoothAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'location', 'operator', 'show_operation_times')
-    list_filter = ('type', 'location', 'operator')
+    list_display = ('name', 'type', 'operator', 'show_operation_times')
+    list_filter = ('type', 'operator')
     search_fields = ('name', 'description', 'operator')
-    inlines = [BoothOperationTimeInline, BoothImageInline]
+    inlines = [BoothLocationOperationTimeInline, BoothImageInline]
 
     def show_operation_times(self, obj):
-        operation_times = obj.operation_times.all()
+        operation_times = obj.location_operation_times.all()
         if operation_times:
             return ', '.join([f"{time.date.strftime('%A')} {time.start_time.strftime('%H:%M')} ~ {time.end_time.strftime('%H:%M')}" for time in operation_times])
         else:
