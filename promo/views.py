c2b6f94c6e7d5ceccab1f7438859115781921be0
Jongbeom Lee <116253832+JongbeomLee623@users.noreply.github.com>
2024-05-27 00:05:09 +0900

    Revert "🚑 Hotfix : 쿼리셋 정렬, 비밀번호 필드 정규식 추가"

--- promo/views.py
+++ promo/views.py
@@ -6,7 +6,7 @@ from .serializers import *
 
 # 디테일 페이지가 필요치 않으므로 list만 구현
 class PromotionViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    queryset = Promotion.objects.all().order_by('-id')
+    queryset = Promotion.objects.all()
     serializer_class = PromotionSerializer
 
 class PromotionBannerViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
