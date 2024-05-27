32afede1190b22c290b7f9186aa45022952ce755
chaeunho1227 <chaeunho2002@gmail.com>
2024-05-27 15:35:47 +0900

    🎨 fix : middleware 위치변경

--- festival/settings.py
+++ festival/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     # 'corsheaders.middleware.CorsMiddleware',
-    'booth.middlewares.ClientTokenMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'booth.middlewares.ClientTokenMiddleware',
 ]
 
 ROOT_URLCONF = 'festival.urls'
